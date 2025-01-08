b1f47e6101ee77e4a7993d3dfc01d0a3b067d200
eden <eedenbaui@gmail.com>
2025-01-08 18:50:24 +0800

    chore: add material-icons package, modify theme, add sample data

--- app.vue
+++ app.vue
@@ -5,3 +5,19 @@
         </NuxtLayout>
     </div>
 </template>
+
+<script lang="ts">
+import 'material-icons/iconfont/material-icons.css';
+
+</script>
+
+<style>
+html,
+body,
+main,
+#__nuxt,
+#__nuxt>div {
+    height: 100%;
+    margin: 0;
+}
+</style>
