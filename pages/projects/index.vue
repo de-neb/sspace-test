c4728b9c351dd63a50e2513e27102d4590464d41
eden <eedenbaui@gmail.com>
2025-01-08 18:52:18 +0800

    feat: project page

---
+++ pages/projects/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="w-full h-full flex flex-col">
+        <div class="flex-none  h-1/3 px-5 py-3">
+            <Breadcrumb />
+            <ul>
+                <li
+                    v-for="project in projects"
+                    :key="project.name"
+                >
+                    <NuxtLink
+                        class="text-black/90 hover:text-accent"
+                        :to="`${route.path}/${project.slug}`"
+                    >{{ project.name }}</NuxtLink>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { SAMPLE_PROJECTS } from '~/constants/projects';
+
+const route = useRoute()
+
+const projects = computed((() => SAMPLE_PROJECTS.map((project: string) => ({
+    name: project,
+    slug: project.replaceAll(' ', '-').toLocaleLowerCase()
+}))))
+
+</script>
+
+<style scoped></style>
