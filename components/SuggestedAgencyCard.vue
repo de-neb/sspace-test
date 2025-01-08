bf7d98a71890e38fd2f2f4b3390e9787cce6efcd
eden <eedenbaui@gmail.com>
2025-01-08 18:56:32 +0800

    feat: create SuggestedAgencyCard and SuggestedAgencyList

---
+++ components/SuggestedAgencyCard.vue
@@ -0,0 +1,64 @@
+<template>
+    <Card>
+        <template #banner>
+            <img
+                :src="agency.agencyBannerImage"
+                alt=""
+            >
+        </template>
+
+        <template #body>
+            <div class="flex gap-3">
+                <img
+                    class="w-[80px] rounded-md"
+                    :src="agency.agencyLogoImage"
+                >
+                <div class="mr-auto flex flex-col gap-1">
+                    <h2 class="font-bold text-xl">{{ agency.agencyName }}</h2>
+                    <p class="uppercase text-slate-500 text-xs">{{ agency.agencyLocations.join(", ") }}</p>
+                    <span class="text-info flex items-center gap-1">
+                        <span class="material-icons !text-md">
+                            info
+                        </span>
+                        Suggested by StudioSpace
+                    </span>
+                </div>
+            </div>
+        </template>
+
+        <template #actions>
+            <button
+                class="btn btn-ghost text-[teal] font-bold w-full flex items-center justify-start"
+                @click="emit('addToShortlist')"
+            >
+                <span class="material-icons">
+                    add
+                </span>
+                Add to Shortlist
+            </button>
+        </template>
+    </Card>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['addToShortlist'])
+defineProps({
+    agency: {
+        type: Object,
+        default: {
+            "addedBy": "studiospace",
+            "agencyBannerImage": "https://stmp-prod.ams3.cdn.digitaloceanspaces.com/1721146683561_AMAN_NY_JH_LIFESTYLE_13786_2_copy.jpg",
+            "agencyExternalLink": "https://google.com",
+            "agencyId": "38423-12412497",
+            "agencyLocations": [
+                "Australia",
+                "United Kingdom"
+            ],
+            "agencyLogoImage": "https://stmp-prod.ams3.cdn.digitaloceanspaces.com/1721146238694_SOW_BLACK_LOGO.jpg",
+            "agencyName": "Test Agency Name"
+        }
+    }
+})
+</script>
+
+<style scoped></style>
---
+++ components/SuggestedAgencyList.vue
@@ -0,0 +1,34 @@
+<template>
+    <Collapse>
+        <template #title>
+            <h6 class="uppercase font-semibold">Suggested Agencies ({{ suggestedAgenciesLength }})</h6>
+        </template>
+
+        <template #content>
+            <div class="flex gap-4 flex-wrap ">
+                <span
+                    class="text-slate-400 mx-auto"
+                    v-if="!suggestedAgenciesLength"
+                >No Suggested Agencies to show.</span>
+
+                <SuggestedAgencyCard
+                    class="mb-4"
+                    v-for="suggestedAgency in suggestedAgencies"
+                    :key="suggestedAgency.agencyId"
+                    :agency="suggestedAgency"
+                    @addToShortlist="addAgencyToShortList(suggestedAgency)"
+                ></SuggestedAgencyCard>
+            </div>
+        </template>
+    </Collapse>
+</template>
+
+<script setup lang="ts">
+import { useAgencyState } from '#build/imports';
+
+const { agencies: suggestedAgencies, addAgencyToShortList } = useAgencyState()
+
+const suggestedAgenciesLength = computed(() => suggestedAgencies.value.length)
+</script>
+
+<style scoped></style>
